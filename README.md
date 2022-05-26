bfd4e5c6c06af4db51fb4e498782f65a37edd2f3
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2022-05-26 18:00:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# reals
+
+A lightweight python3 library for arithmetic with real numbers.
+
+```
+from reals import pi, e
+from reals.undecidable import correct_digits
+
+# print 1000 digits of e * pi
+print(correct_digits(e * pi, 1000))
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='reals',
+    version='0.0.1-alpha',
+    author='Ruben van Nieuwpoort',
+    author_email='ruben@vannieuwpoort.dev',
+    license='MIT',
+    description='A lightweight python3 library for arithmetic with real numbers.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rubenvannieuwpoort/reals',
+    project_urls={
+        'Bug Tracker': 'https://github.com/rubenvannieuwpoort/reals/issues',
+    },
+    packages=['reals'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)

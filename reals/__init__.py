9343816f9eb397a34db9cb3e748582edf5279ff1
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2022-09-05 22:07:23 +0200

    Implement sin, cos, tan

--- reals/__init__.py
+++ reals/__init__.py
@@ -2,5 +2,6 @@ from reals._real import Real
 from reals._constants import e, phi, pi
 from reals._exponential import exp
 from reals._sqrt import sqrt
+from reals._trigonometric import sin, cos, tan
 
-__all__ = ['Real', 'e', 'phi', 'pi', 'exp', 'sqrt']
+__all__ = ['Real', 'e', 'phi', 'pi', 'exp', 'sqrt', 'sin', 'cos', 'tan']

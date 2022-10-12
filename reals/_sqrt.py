56d89aca5983c298eeb0cd71f23c652a6e85cea8
Ruben van Nieuwpoort <38811037+rubenvannieuwpoort@users.noreply.github.com>
2022-10-12 09:38:55 +0200

    Fix sqrt (#14)

--- reals/_sqrt.py
+++ reals/_sqrt.py
@@ -1,10 +1,11 @@
-import reals._real
+from reals._real import Real
 import reals._computation
 import reals._quadratic_computation
 import reals._homographic
 import reals._term
 
 from fractions import Fraction
+from decimal import Decimal
 from typing import Union
 
 
@@ -30,11 +31,11 @@ class SqrtComputation(reals._computation.Computation):
         return n
 
 
-def sqrt(f: Union[Fraction, int]) -> reals._real.Real:
+def sqrt_frac(f: Union[Fraction, int, Decimal]) -> Real:
     p, q = f.as_integer_ratio()
 
     if p == 0:
-        return reals._real.Real.from_int(0)
+        return Real.from_int(0)
 
     if p < 0:
         raise ValueError()
@@ -42,16 +43,24 @@ def sqrt(f: Union[Fraction, int]) -> reals._real.Real:
     assert p > 0
 
     if q == 1:
-        return reals._real.Real(SqrtComputation(p))
+        return Real(SqrtComputation(p))
 
     # simply returning SqrtComputation(Homographic(0, p, q, 0)) works for most fractions except when p, q are squares
     # and q > 1 for q = 1 and p square -- a hack is in place which seems to work in practice, so this way of computing
     # works when p and q > 1 are squares
 
     # this is a variation on Real(SqrtComputation(p)) / Real(SqrtComputation(q)) with less overhead
-    return reals._real.Real(
+    return Real(
         reals._quadratic_computation.QuadraticComputation(
             SqrtComputation(p),
             SqrtComputation(q),
             (0, 1, 0, 0, 0, 0, 1, 0)
         ))
+
+
+def sqrt(x: Union[Real, Fraction, int, Decimal]) -> Real:
+    if isinstance(x, Real):
+        return x.__pow__(Fraction(1, 2))
+    if isinstance(x, Fraction) or isinstance(x, int) or isinstance(x, Decimal):
+        return sqrt_frac(x)
+    raise TypeError()

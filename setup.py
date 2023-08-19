6969ff6871d8d93b2f5970384242c56854c48a63
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-08-19 13:09:00 +0200

    Prepare 0.0.5 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='reals',
-    version='0.0.4',
+    version='0.0.5',
     author='Ruben van Nieuwpoort',
     author_email='ruben@vannieuwpoort.dev',
     license='MIT',

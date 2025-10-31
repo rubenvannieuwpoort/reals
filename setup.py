2bcbe1a559749b25aab5138e25c5e2e0da857405
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-10-31 11:14:36 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='reals',
-    version='0.0.5',
+    version='0.0.6',
     author='Ruben van Nieuwpoort',
     author_email='ruben@vannieuwpoort.dev',
     license='MIT',
